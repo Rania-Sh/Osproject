29db534c170b882df28ea82567473c1aeecedac3
Rania-Sh <shqeratra@post.jce.ac.il>
2026-05-02 17:54:45 +0300

    Update animationui.c

--- animationui.c
+++ animationui.c
@@ -79,28 +79,4 @@ bool drawButton(Rectangle rect, const char *label,
 
     return hover && IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
 }
-int findShortestPath(Graph *g, int src, int dst, int *path) {
-    int n = g->numVertices;
-    int dist[64], prev[64], visited[64];
-    for (int i = 0; i < n; i++) {
-        dist[i] = 1000000; prev[i] = -1; visited[i] = 0;
-    }
-    dist[src] = 0;
-    for (int iter = 0; iter < n; iter++) {
-        int u = -1;
-        for (int i = 0; i < n; i++)
-            if (!visited[i] && (u == -1 || dist[i] < dist[u])) u = i;
-        if (u == -1 || dist[u] == 1000000) break;
-        visited[u] = 1;
-        for (int v = 0; v < n; v++) {
-            if (g->weights[u][v] > 0) {
-                int nd = dist[u] + g->weights[u][v];
-                if (nd < dist[v]) { dist[v] = nd; prev[v] = u; }
-            }
-        }
-    }
-    int tmp[64], len = 0, cur = dst;
-    while (cur != -1) { tmp[len++] = cur; cur = prev[cur]; }
-    for (int i = 0; i < len; i++) path[i] = tmp[len - 1 - i];
-    return (len > 0 && path[0] == src) ? len : 0;
-}
+
