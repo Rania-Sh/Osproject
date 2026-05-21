00815c9206853dc63fec970b994ffcfbdea44b76
Rania-Sh <shqeratra@post.jce.ac.il>
2026-05-21 10:31:20 +0300

    Add loadGraphAndTravelers function prototype

--- animationui.h
+++ animationui.h
@@ -8,20 +8,18 @@
 
 #include "raylib.h"
 #include <stdbool.h>
-
-
 #include "animation.h"
 
 Graph *loadGraph(const char *filename, int *numNodes);
+Graph *loadGraphAndTravelers(const char *filename, int *numNodes,
+                              int *numTravelers,
+                              int *sources, int *dests);
+ 
 void freeGraph(Graph *g);
-
-
-
-
 void defaultLayout(int n, Point *pos);
-
 bool drawButton(Rectangle rect, const char *label,
                 Color bg, Color fg, bool hover);
+
 
 
 #endif //OSPROJECT_ANIMATIONUI_H
